52b257477c996f56b1155447b924118458f50d24
rachel-source <rachel_rebhun@yahoo.fr>
2019-08-14 00:08:15 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+}
